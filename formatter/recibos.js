5bb0f8249bcb1b7f53f9c8bb404dce19f0a8a26d
Jose <jmpumero@gmail.com>
2022-10-23 21:53:24 -0400

    new

--- formatter/recibos.js
+++ formatter/recibos.js
@@ -14,13 +14,13 @@ function generateRow(configDoc,doc){
     });
   
     //console.log(metaData);
-    row=shareModule.buildRow(row,doc.FECHA_RECIBO,1,metaData.fecha_operacion.longitud)
+    row=shareModule.buildRow(row,doc.FECHA_RECIBO,1,metaData.fecha_operacion.longitud)//*
     row=shareModule.buildRow(row,doc.NUMERO_REFERENCIA,12,metaData.referencia.longitud)
-    row=shareModule.buildRow(row,doc.BANCO,23,metaData.banco.longitud)
-    row=shareModule.buildRow(row,doc.NUMERO_CUENTA_BANCARIA,39,metaData.cuenta.longitud)
-    row=shareModule.buildRow(row,doc.RIF_CLIENTE + doc.RAZON_SOCIAL_CLIENTE,60,metaData.descripcion.longitud)
-    row=shareModule.buildRow(row,doc.CODIGO_DE_CLIENTE,96,metaData.codigo_cliente.longitud)
-    row=shareModule.buildRow(row,doc.MONEDA,107,metaData.divisa.longitud)
+    row=shareModule.buildRow(row,doc.BANCO,23,metaData.banco.longitud)//*
+    row=shareModule.buildRow(row,doc.NUMERO_CUENTA_BANCARIA,39,metaData.cuenta.longitud)//*
+    row=shareModule.buildRow(row,doc.RIF_CLIENTE + doc.RAZON_SOCIAL_CLIENTE,60,metaData.descripcion.longitud)//*
+    row=shareModule.buildRow(row,doc.CODIGO_DE_CLIENTE,96,metaData.codigo_cliente.longitud)//*
+    row=shareModule.buildRow(row,doc.MONEDA,107,metaData.divisa.longitud)//*
     row=shareModule.buildRow(row,doc.VENDEDOR,111,metaData.vendedor.longitud)
     row=shareModule.buildRow(row,doc.NOMBRE_ZONA,147,metaData.nombre_zona.longitud)
     row=shareModule.buildRow(row,doc.USUARIO_AFV,158,metaData.usuario_afv.longitud)
@@ -31,12 +31,12 @@ function generateRow(configDoc,doc){
     
     
     if (doc.MONEDA==='USD'){
-      row=shareModule.buildRow(row,shareModule.transformTransationImport(doc.MONTO_DOLAR),199,metaData.importe_transaccion.longitud,metaData.importe_transaccion.alineacion)
+      row=shareModule.buildRow(row,shareModule.transformTransationImport(doc.MONTO_DOLAR),199,metaData.importe_transaccion.longitud,metaData.importe_transaccion.alineacion)//*
     }else{
       
-      row=shareModule.buildRow(row,shareModule.transformTransationImport(doc.MONTO_BS),199,metaData.importe_transaccion.longitud,metaData.importe_transaccion.alineacion)
+      row=shareModule.buildRow(row,shareModule.transformTransationImport(doc.MONTO_BS),199,metaData.importe_transaccion.longitud,metaData.importe_transaccion.alineacion)//*
     }
-    row=shareModule.buildRow(row,'+',218,metaData.sentido.longitud)
+    row=shareModule.buildRow(row,'+',218,metaData.sentido.longitud)//*
   
     row=shareModule.buildRow(row,shareModule.transformInvoice(doc.FACTURAS),220,metaData.facturas.longitud)
   
